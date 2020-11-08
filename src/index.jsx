5122a7695d363c49d271fe30d7fc1486bb91d56e
Tereza Bayerová <49003875+TerezaBay@users.noreply.github.com>
2020-11-08 19:42:55 +0100

    Úprava hlavního JSX - App a render

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.html';
 
-render(
-  <>
-    <header>
-      <h1>React Starter</h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app')
-);
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>test</h1>
+      </header>
+      <main>
+        <p>Moje první React stránka</p>
+      </main>
+      <footer>Martin Podloucký</footer>
+    </>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
