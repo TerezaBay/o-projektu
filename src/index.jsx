0187e78242a72ca8114a3f33507f30a58b7a27db
Tereza Bayerová <49003875+TerezaBay@users.noreply.github.com>
2020-11-13 18:22:36 +0100

    Přidání komponenty SectionAuthors - zatím bez map

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import { render } from 'react-dom';
 import './index.html';
 import './style.css';
 
-import { homepageTexts } from './texts.jsx';
+import { homepage } from './texts.jsx';
 import Header from './Header/index.jsx';
 
 const SectionQuote = ({ text }) => (
   <>
     <section className="section_guote">
-      <div className="section_text">{text}</div>
+      <div className="quote_text">{text}</div>
     </section>
   </>
 );
@@ -21,12 +21,27 @@ const HomepageMain = ({ titleText, listText, emphaText }) => (
         <h3 className="main_title-text">{titleText}</h3>
       </div>
       <ul className="main_list">
-        {
-        listText.map((list) => <li>{list}</li>)
-        }
+        {listText.map((list) => (
+          <li>{list}</li>
+        ))}
       </ul>
       <p className="main_emphasized-text">{emphaText}</p>
     </main>
+  </>
+);
+
+const SectionAuthors = ({ titleText, image, name, text }) => (
+  <>
+    <section className="section_authors">
+      <div className="authors_title-div">
+        <h3 className="authors_title-text">{titleText}</h3>
+      </div>
+      <div className="author-div">
+        <img src={image} alt={`Fotografie - ${name}`} />
+        <p className="author_name">{name}</p>
+        <p className="author_text">{text}</p>
+      </div>
+    </section>
   </>
 );
 
@@ -34,19 +49,25 @@ const App = () => {
   return (
     <>
       <Header
-        logo={homepageTexts.logo}
-        buttonText1={homepageTexts.headerButton1}
-        buttonText2={homepageTexts.headerButton2}
-        title={homepageTexts.headerTitle}
-        titleText={homepageTexts.headerText}
+        logo={homepage.logo}
+        buttonText1={homepage.headerButton1}
+        buttonText2={homepage.headerButton2}
+        title={homepage.headerTitle}
+        titleText={homepage.headerText}
       />
-      <SectionQuote text={homepageTexts.section1} />
+      <SectionQuote text={homepage.section1} />
       <HomepageMain
-        titleText={homepageTexts.mainTitleText}
-        listText={homepageTexts.mainListText}
-        emphaText={homepageTexts.mainEmphaText}
+        titleText={homepage.mainTitleText}
+        listText={homepage.mainListText}
+        emphaText={homepage.mainEmphaText}
       />
-      <SectionQuote text={homepageTexts.section2} />
+      <SectionQuote text={homepage.section2} />
+      <SectionAuthors
+        titleText={homepage.authors[0].authorsTitleText}
+        image={homepage.authors[0].authorsImg}
+        name={homepage.authors[0].authorsName}
+        text={homepage.authors[0].authorsText}
+      />
 
       <footer>Toto čeká na Věrku</footer>
     </>
--- src/texts.jsx
+++ src/texts.jsx
@@ -1,4 +1,4 @@
-export const homepageTexts = {
+export const homepage = {
   logo: 'Moje porodní přání',
   headerButton1: 'Porodní přání',
   headerButton2: 'Užitečné informace',
@@ -20,4 +20,19 @@ export const homepageTexts = {
   ],
   mainEmphaText:
     'Výstupem z aplikace by pro uživatele měl být přehledný porodní plán, který si bude moci vzít do porodnice.',
+
+  authors: [
+    {
+      authorsTitleText: "O autorkách projektu",
+      authorsImg: "http://placekitten.com/g/200/300", // tady bude třeba vložit naše obrázky a cestu k nim
+      authorsName: "Věra Staňková",
+      authorsText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure magni nam facilis distinctio nobis facere. Rerum, quo aliquid id officiis maxime error? Quidem, necessitatibus corrupti." // tady bude třeba dopsat text
+    },
+    {
+      authorsTitleText: "O autorkách projektu",
+      authorsImg: "http://placekitten.com/g/200/300", // tady bude třeba vložit naše obrázky a cestu k nim
+      authorsName: "Tereza Bařinková",
+      authorsText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo odio dolorum reprehenderit officia minima facere, iusto aliquid necessitatibus ipsum in deserunt, non suscipit tenetur, nulla quasi corporis doloribus id quibusdam?", // tady bude třeba dopsat text
+    }
+  ]
 };
