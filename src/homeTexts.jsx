5c2a3ac091d955016c1b276503fb035cdee010cb
Věra Staňková <stankova.verka@gmail.com>
2020-11-14 22:14:47 +0100

    upravený text o mě + malé úpravy dalších textů

--- src/homeTexts.jsx
+++ src/homeTexts.jsx
@@ -1,31 +1,31 @@
-import TerkaUrl from "./img/Terka.jpg"
-import VeraUrl from "./img/vera.jpg"
+import TerkaUrl from './img/Terka.jpg';
+import VeraUrl from './img/vera.jpg';
 
 export const home = {
   logo: 'Moje porodní přání',
   headerButton1: 'O aplikaci',
-  button1Url: "#main",
+  button1Url: '#main',
   headerButton2: 'O autorkách',
-  button2Url: "#authors",
+  button2Url: '#authors',
   headerButton3: 'Sestavit porodní přání',
   headerTitle: 'Pro lepší zážitek z porodu',
   headerText: 'Aplikace pro sestavení porodního přání',
 
   section1:
-    'Chtěly bychom vytvořit aplikaci Moje porodní přání, ve které si budoucí rodiče budou zdarma moci vytvořit porodní plán ušitý na míru jejich preferencím a potřebám.',
+    'V aplikaci "Moje porodní přání" si nastávající rodiče zdarma vytvoří porodní plán na míru svým osobním preferencím a potřebám.',
   section2: 'Na tom, jak přicházíme na svět, záleží.',
 
   mainTitleText: 'Aplikace Moje porodní přání rodičům pomůže:',
   mainListText: [
-    'Sestavit si porodní plán podle svých potřeb a představ.',
-    'Informovat se o jednotlivých možnostech.',
-    'Zjistit, co je pro ně u porodu opravdu důležité.',
-    'Sdílet porodní plán s ostatními.',
-    'Uložit si rozpracovaný porodní plán a vrátit se k němu později.',
-    'Hotový plán si jednoduše vytisknout dle předpřipravené šablony.',
+    'sestavit si porodní plán podle svých potřeb a představ,',
+    'informovat se o jednotlivých možnostech,',
+    'zjistit, co je pro ně u porodu opravdu důležité,',
+    'sdílet porodní plán s ostatními,',
+    'uložit si rozpracovaný porodní plán a vrátit se k němu později,',
+    'hotový plán si jednoduše vytisknout dle předpřipravené šablony.',
   ],
   mainEmphaText:
-    'Výstupem z aplikace by pro uživatele měl být přehledný porodní plán, který si bude moci vzít do porodnice.',
+    'Výstupem z aplikace bude přehledný a reprezentativní porodní plán, který si uživatel bude moci vzít s sebou do porodnice.',
 
   authorsTitleText: 'O autorkách projektu:',
   authors: [
@@ -33,7 +33,7 @@ export const home = {
       authorsImg: VeraUrl, // tady bude třeba vložit naše obrázky a cestu k nim
       authorsName: 'Věra Staňková',
       authorsText:
-        'Věrka pracuje pro skvělou neziskovku Aperio. Je hlavním nositelem nápadu a určitě se vyřádí na stylování!',
+        'Věrka pracuje pro skvělou neziskovku Aperio. Je hlavním nositelem nápadu a určitě se vyřádí na stylování! Má velikou radost, že projekt začíná dostávat pevné obrysy a doufá, že aplikace brzy pomůže prvním nastávajícím rodičům s přípravou na porod.',
     },
     {
       authorsImg: TerkaUrl, // tady bude třeba vložit naše obrázky a cestu k nim
